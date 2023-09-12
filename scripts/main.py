75383874e44a51a817f4702075cddfa308779ce6
hiro-oh <ohshima.hiroyuki2911@gmail.com>
2023-09-12 12:06:53 +0900

    add: 実行したら動くが，終了しない

--- scripts/main.py
+++ scripts/main.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
-import rospy
+import rospy, os, getpass, subprocess
 from geometry_msgs.msg import Twist
-import keyboard
 
 class Turtlesim_task():
     def __init__(self):
@@ -18,35 +17,22 @@ class Turtlesim_task():
         t.angular.z = 0.0
         self.turtlesim_pub.publish(t)
 
-def on_key_event(e):
-    if e.name == 'q' and e.event_type == keyboard.KEY_DOWN:
-        print("終了します...")
-        keyboard.unhook_all()  # イベントのフックを解除してプログラムを終了
-    else:
-        print(f'キー {e.name} が {e.event_type} されました')
-
-
 if __name__=="__main__":
     rospy.init_node("turtlesim_main_node")
 
     turtlesim_task = Turtlesim_task()
 
     rate = rospy.Rate(1)
-    keyboard.hook(on_key_event)
 
+    user_name = os.getlogin()
+    keyboard_file_path = '/home/' + user_name + '/catkin_ws/src/turtlesim_task/scripts/keyboard_event.py'
+
+    command = "sudo -S python3 " + keyboard_file_path
+    password = (getpass.getpass() + '\n').encode()
+    subprocess.run(command.split(), input=password)
+    
+    
     while not rospy.is_shutdown():
         turtlesim_task.publish()
 
         rate.sleep()
-
-    # try:
-    #     while True:
-    #         key = input("入力：　") 
-
-    #         if key == "q":
-    #             print("終了する")
-    #             break
-    #         else:
-    #             print("何もしない")
-    # except KeyboardInterrupt:
-    #     pass
