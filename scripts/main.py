2e7189065973d650710866a55f3bd48aab958c9f
hiro-oh <ohshima.hiroyuki2911@gmail.com>
2023-09-13 22:33:51 +0900

    add: can do both to get keyboard key and to order turtlesim

--- scripts/main.py
+++ scripts/main.py
@@ -1,38 +1,57 @@
 #!/usr/bin/env python3
 
-import rospy, os, getpass, subprocess
+import rospy, sys, threading
 from geometry_msgs.msg import Twist
 
 class Turtlesim_task():
     def __init__(self):
         self.turtlesim_pub = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size=10)
 
-    def publish(self):
+    def publish(self, linear_x=1.0, linear_y=0.0, linear_z=0.0, angle_x=0.0, angle_y=0.0, angle_z=0.0):
         t = Twist()
-        t.linear.x = 0.0
-        t.linear.y = 0.0
-        t.linear.z = 0.0
-        t.angular.x = 0.0
-        t.angular.y = 0.0
-        t.angular.z = 0.0
+        t.linear.x = linear_x
+        t.linear.y = linear_y
+        t.linear.z = linear_z
+        t.angular.x = angle_x
+        t.angular.y = angle_y
+        t.angular.z = angle_z
         self.turtlesim_pub.publish(t)
+        print("publish turtlesim data")
 
-if __name__=="__main__":
-    rospy.init_node("turtlesim_main_node")
-
+def turtlesim_main():
     turtlesim_task = Turtlesim_task()
 
     rate = rospy.Rate(1)
 
-    user_name = os.getlogin()
-    keyboard_file_path = '/home/' + user_name + '/catkin_ws/src/turtlesim_task/scripts/keyboard_event.py'
-
-    command = "sudo -S python3 " + keyboard_file_path
-    password = (getpass.getpass() + '\n').encode()
-    subprocess.run(command.split(), input=password)
-    
-    
     while not rospy.is_shutdown():
-        turtlesim_task.publish()
+        turtlesim_task.publish(1,1,1,1,1,1)
 
         rate.sleep()
+
+def get_key():
+    shutdown_message = "------------------------\n  main system shutdown\n------------------------"
+
+    while True:
+        print("keyboard event searching...")
+        key = sys.stdin.read(1)
+        if key == 'q':
+            print(shutdown_message)
+            rospy.signal_shutdown("Shutdown")
+            break
+
+if __name__=="__main__":
+    rospy.init_node("turtlesim_main_node")
+
+    try:
+        turtlesim_thread = threading.Thread(target=turtlesim_main)
+        turtlesim_thread.daemon = True
+        turtlesim_thread.start()
+
+        keyboard_thread = threading.Thread(target=get_key)
+        keyboard_thread.daemon = True
+        keyboard_thread.start()
+
+        rospy.spin()
+
+    except:
+        print("System error happen.")
